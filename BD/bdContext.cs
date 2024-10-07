339b72f7ca465757baf71c5fd183ba382c87a31c
KevinMeddz <kvnm.1011@gmail.com>
2024-10-06 18:43:15 -0600

    Se elimina la carpeta BD

--- BD/bdContext.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SistemaOrdenes.BD
-{
-    public class bdContext
-    {
-        public static string CN = "Server=localhost; Database=DB_PruebaOrdenes ; Trusted_Connection=True; Encrypt=False;";
-    }
-}
-  
