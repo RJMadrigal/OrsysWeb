81712c1a7bc566283ed15b2c772b1087fd796701
KevinMeddz <kvnm.1011@gmail.com>
2024-10-09 17:50:06 -0600

    Vista de jefe aprobador y financiero

---
+++ Controllers/OrdenesJefesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaOrdenes.Controllers
+{
+    public class OrdenesJefesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Reportes()
+        {
+            return View();
+        }
+
+        public IActionResult Revisar()
+        {
+            return View();
+        }
+    }
+}
